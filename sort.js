618f055e3d2326d9f1c39ec858402f3abb0ae0b2
Lezeto <nicolas0vf@gmail.com>
2026-01-05 23:28:29 -0300

    first commit

---
+++ sort.js
@@ -0,0 +1,42 @@
+function bubbleSort(arr) {
+    let array = [...arr];
+
+    for (let i = 0; i < array.length; i++) {
+        for (let j = 0; j < array.length - i - 1; j++) {
+            if (array[j] > array[j + 1]) {
+                // Swap
+                let temp = array[j];
+                array[j] = array[j + 1];
+                array[j + 1] = temp;
+            }
+        }
+    }
+
+    return array;
+}
+
+function quickSort(arr) {
+    if (arr.length <= 1) {
+        return arr;
+    }
+
+    let pivot = arr[arr.length - 1];
+    let left = [];
+    let right = [];
+
+    for (let i = 0; i < arr.length - 1; i++) {
+        if (arr[i] < pivot) {
+            left.push(arr[i]);
+        } else {
+            right.push(arr[i]);
+        }
+    }
+
+    return [...quickSort(left), pivot, ...quickSort(right)];
+}
+
+let numbers = [5, 3, 8, 4, 2, 7, 1, 10];
+
+console.log("Original array:", numbers);
+console.log("Bubble Sort:", bubbleSort(numbers));
+console.log("Quick Sort:", quickSort(numbers));
